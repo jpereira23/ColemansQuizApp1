39954d150347533cf8b2872a9aa4b27bbaeea24b
jpereira23 <jpereira1@mail.csuchico.edu>
2017-06-06 15:33:47 -0700

    This works so far

--- Quiz/ViewController.swift
+++ Quiz/ViewController.swift
@@ -66,6 +66,12 @@ class ViewController: UIViewController {
         super.viewDidLoad()
         questionLabel.text = questions[currentQuestionIndex]
     }
+    
+    @IBAction func unwindToViewController(_ sender: UIStoryboardSegue)
+    {
+        
+    }
+    
 //After the application is launched, you will want to load he first question from the array and
 //use it to replace the ??? placeholder in the questionLabel labe. A good way to do this is by 
 //overriding the viewDidLoad() method of viewController. ("Override" means that you are providing a
